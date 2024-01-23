31a359f31c558dbda5300acf2757ca24df40e9c5
Aleksandar Stajic <aleksandar-stajic@outlook.com>
2024-01-23 15:55:02 +0100

    1431. kids with the greatest number of candies

---
+++ src/tests/java/ArraysAndHashing/GreatestCommonDivisorOfStringsTest.java
@@ -0,0 +1,6 @@
+package ArraysAndHashing;
+
+import static org.junit.jupiter.api.Assertions.*;
+class GreatestCommonDivisorOfStringsTest {
+  
+}
---
+++ src/tests/java/ArraysAndHashing/KidsWithTheGreatestNumberOfCandiesTest.java
@@ -0,0 +1,7 @@
+package ArraysAndHashing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KidsWithTheGreatestNumberOfCandiesTest {
+
+}
